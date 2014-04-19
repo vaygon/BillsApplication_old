5be3ce67dde1d882bb6142827a85d919a2479cd4
vaygon <leemargiotta@gmail.com>
2014-04-19 11:59:35 +0100

    Configer and Setup Ninject

--- BillsApplication/BillsApplication/Global.asax.cs
+++ BillsApplication/BillsApplication/Global.asax.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using BillsApplicationDomain.Services;
+using Ninject;
+using Ninject.Web.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -8,14 +12,24 @@ using System.Web.Routing;
 
 namespace BillsApplication
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : NinjectHttpApplication
     {
-        protected void Application_Start()
+        protected override void OnApplicationStarted()
         {
+            base.OnApplicationStarted();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected override Ninject.IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            kernel.Bind<IBillService>().To<BillService>();
+
+            return kernel;
         }
     }
 }
